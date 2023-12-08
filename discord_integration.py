c48b76598aa603e00cd588b66a119e901ed434e4
mak448a <mak448a@gmail.com>
2023-12-07 20:10:20 -0600

    Add project files

---
+++ discord_integration.py
@@ -0,0 +1,79 @@
+import requests
+import json
+
+people = set()
+
+
+def get_messages(channel_id, beforemessage=None):
+    """Returns messages and then last message id"""
+    with open("discordauth.txt") as f:
+        auth = f.read()
+
+    headers = {
+        "authorization": f"{auth}"
+    }
+    if beforemessage:
+        r = requests.get(f"https://discord.com/api/v9/channels/{channel_id}/messages?before={beforemessage}&limit=10",
+                         headers=headers)
+    else:
+        r = requests.get(f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=10", headers=headers)
+    jsonn = json.loads(r.text)
+    new_list = []
+    for value in jsonn:
+        if not value["content"]:
+            people.add(value["author"]["global_name"])
+            new_list.append(
+                {"username": value["author"]["global_name"], "content": "[NOT A TEXT (call/image)]", "id": value["id"]})
+        else:
+            # people.add(value["author"]["username"])
+            people.add(value["author"]["global_name"])
+            # new_list.append({"username": value["author"]["username"], "content": value["content"], "id": value["id"]})
+            new_list.append(
+                {"username": value["author"]["global_name"], "content": value["content"], "id": value["id"]})
+            # print(value, "\n")
+
+    newnewlist = new_list.copy()
+    # Reversed list!
+    new_list.reverse()
+    last_message = new_list[0]["id"]
+    # print(new_list)
+    return new_list, last_message
+
+
+def send_message(msg, channel):
+    with open("discordauth.txt") as f:
+        auth = f.read()
+    headers = {
+        "authorization": f"{auth}"
+    }
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel}/messages",
+                      headers=headers,
+                      json={"content": msg})
+    # print(r.text)
+
+
+def get_friends():
+    with open("discordauth.txt") as f:
+        auth = f.read()
+    headers = {
+        "authorization": f"{auth}"
+    }
+    r = requests.get(f"https://discord.com/api/v9/users/@me/relationships",
+                      headers=headers)
+    
+    # for friend in r.json():
+        
+    #     print(friend["user"]["global_name"])
+    
+    return r.json()
+
+
+def get_channel_from_id(user_id):
+    with open("discordauth.txt") as f:
+        auth = f.read()
+    headers = {
+        "authorization": f"{auth}"
+    }
+    r = requests.post(f"https://discord.com/api/v9/users/@me/channels",
+                      headers=headers, json={"recipient_id": user_id})
+    return r.json()["id"]
---
+++ main.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python3
+import os
+os.system("pyside6-uic main.ui -o main_ui.py")  # NOQA (basically tells pycharm to shut up)
+
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QMessageBox
+)
+
+from main_ui import Ui_MainWindow
+from PySide6.QtGui import QShortcut, QKeySequence, QIcon
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QPushButton
+import discord_integration
+from PySide6.QtCore import QRunnable, Slot, QThreadPool
+import time
+
+
+
+# Use worker thread
+# https://www.pythonguis.com/tutorials/multithreading-pyside-applications-qthreadpool/
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    quit_shortcut = None
+    response, prev_response = "", ""
+    messages = ""
+    friends = []
+    channel = 0
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.setup()
+
+
+    def connect_signal_slots(self):
+        self.ui.pushButton.clicked.connect(self.handle_input)
+
+        self.ui.actionQuit.triggered.connect(quit)
+        self.ui.actionAbout.triggered.connect(self.about)
+        # Shortcuts
+        # Quit
+        self.quit_shortcut = QShortcut(QKeySequence("Ctrl+Q"), self)
+        self.quit_shortcut.activated.connect(quit)
+
+        # Detect enter key by using event filter function from self.
+        self.ui.lineEdit.returnPressed.connect(self.handle_input)
+
+    def about(self):
+        QMessageBox.about(
+            self,
+            "About ChatApp",
+            "<p>ChatApp (c) mak448a 2023</p>"
+            "<p>This app uses the following</p>"
+            "<p>- PySide6</p>"
+            "<p>- Qt Designer</p>"
+            "<p style='color:blue; background: rgb(30,30,30)'>- <strong>Python</strong></p>"
+            "<p>PySide is licensed under LGPL.</p>",
+        )
+
+    def handle_input(self):
+        text = self.ui.lineEdit.text()
+        if text:
+            self.ui.lineEdit.setText("")
+
+            self.messages += f"You: {text}\n"
+
+            discord_integration.send_message(text, self.channel)
+            
+            self.update_text()
+    
+    def update_text(self):
+        if not self.channel:
+            return
+        
+        
+        # Get messages
+        # if messages aresame, dont update
+        messages = ""
+
+        for message in discord_integration.get_messages(self.channel)[0]:
+            messages += message["username"] + ": " + message["content"] + "\n"
+        if self.messages == messages:
+            return
+        
+        self.messages = messages
+        self.ui.textBrowser.setText(self.messages)
+
+        # Scroll to bottom
+        self.ui.textBrowser.verticalScrollBar().setValue(self.ui.textBrowser.verticalScrollBar().maximum())
+    
+    def setup(self):
+        self.connect_signal_slots()
+
+        self.ui.lineEdit.setFocus()
+
+        # Set timer to run every 4000 ms to update the chat (8 seconds as we don't want banning)
+        self.timer = QTimer()
+        self.timer.setInterval(4000)
+        self.timer.timeout.connect(self.update_text)
+        self.timer.start()
+
+        # Get friends
+        self.get_friends()
+
+    def get_friends(self):
+        for friend in discord_integration.get_friends():
+            self.friends.append({
+                    "global_name": friend["user"]["global_name"],
+                    "channel": discord_integration.get_channel_from_id(friend["id"]),
+                    "nickname": friend["nickname"]
+                })
+        
+        buttons = {}
+        for i, friend in enumerate(self.friends):
+            buttons[i] = QPushButton(text=friend["global_name"])
+            self.ui.friends_tab.layout().addWidget(buttons[i])
+            
+            channel = friend["channel"]
+            # Oh my headache do not touch this code.
+            # But if you do: https://stackoverflow.com/questions/19837486/lambda-in-a-loop
+            buttons[i].clicked.connect((lambda channel=channel: lambda: self.switch_channel(channel))(channel))
+    
+    def switch_channel(self, _id):
+        # print("We were passed in", _id)
+        self.channel = _id
+        self.update_text()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
