323e4e14fe11bcf2bb33cd63ed7bcb2b028ef483
mak448a <mak448a@gmail.com>
2024-02-17 20:42:13 -0600

    Add placeholder package utilties

---
+++ utilities/package_linux.py
@@ -0,0 +1,3 @@
+# Use appimage
+# This file will only be run on Linux, since it needs zipping capabilities
+# TODO: Write out this file
---
+++ utilities/package_windows.py
@@ -0,0 +1,3 @@
+# Use pyinstaller and distribute as zip
+# This file will only be run on Linux, since it needs zipping capabilities
+# TODO: Write out this file
