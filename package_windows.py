da90a683e585e8ec91460685cf9ae780250c7f59
mak448a <mak448a@gmail.com>
2024-02-17 21:08:47 -0600

    Add packaging scripts for Windows

---
+++ package_windows.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+try:
+    import PyInstaller
+    if PyInstaller:
+        print("PyInstaller is good to go!")
+except ModuleNotFoundError:
+    raise Exception("You need pyinstaller installed. Use pip install pyinstaller.")
+
+
+os.system("pyinstaller main.spec")
+
+choice = input("Delete build cache? (N/y) ").strip().lower()
+
+if choice == "yes" or choice == "y":
+    quit()
+else:
+    shutil.rmtree("build")
--- utilities/package_windows.py
+++
@@ -1,3 +0,0 @@
-# Use pyinstaller and distribute as zip
-# This file will only be run on Linux, since it needs zipping capabilities
-# TODO: Write out this file
