88c3135aba2c3375eafe35ae1f9e3a0ee46e0408
mak448a <mak448a@gmail.com>
2024-02-21 20:27:17 -0600

    Update error message

--- package_windows.py
+++ package_windows.py
@@ -7,7 +7,7 @@ try:
     if PyInstaller:
         print("PyInstaller is good to go!")
 except ModuleNotFoundError:
-    raise Exception("You need pyinstaller installed. Use pip install pyinstaller.")
+    raise Exception("You need pyinstaller installed. Make sure to install a version at least 2 months old as to avoid false positives on virus detectors. Use pip install pyinstaller.")
 
 
 os.system("pyinstaller main.spec")
