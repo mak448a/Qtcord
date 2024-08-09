3fbc42c745de291188c9fbb0ebcb49b341ae3cd3
mak448a <mak448a@gmail.com>
2024-08-09 14:55:37 -0500

    Add mac github action

--- helper_scripts/package_mac.py
+++ helper_scripts/package_mac.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 
 try:
@@ -10,11 +9,4 @@ except ModuleNotFoundError:
     raise Exception("You need pyinstaller installed. Make sure to install a version at least 2 months old as to avoid false positives on virus detectors. Use pip install pyinstaller.")
 
 os.chdir("src")
-os.system("pyinstaller ../main.spec")
-
-# choice = input("Delete build cache? (N/y) ").strip().lower()
-
-# if choice == "yes" or choice == "y":
-#     shutil.rmtree("build")
-# else:
-#     quit()
+os.system("pyinstaller ../../main.spec")
