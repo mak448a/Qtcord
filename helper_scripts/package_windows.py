cdfdfb90ce52b220be5ba23f13dc854e2e9909a1
Ryan Ngai <ryanngai21@gmail.com>
2024-12-11 04:11:06 -0500

    removed the change in directory

--- helper_scripts/package_windows.py
+++ helper_scripts/package_windows.py
@@ -11,8 +11,4 @@ except ModuleNotFoundError:
         "You need pyinstaller installed. Make sure to install a version at least 2 months old as to avoid false positives on virus detectors. Use pip install pyinstaller."
     )
 
-current_dir = os.getcwd()
-os.chdir("src")
-new_dir = os.getcwd()
-print(f"Changed from {current_dir} to {new_dir}")
 os.system("pyinstaller ../helper_scripts/win.spec")
