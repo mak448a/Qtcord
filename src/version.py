d525dd9b545e90d93c2749701585a67a840290f1
mak448a <94062293+mak448a@users.noreply.github.com>
2026-05-14 22:40:18 -0500

    Print version in version.py when ran directly

--- src/version.py
+++ src/version.py
@@ -19,3 +19,7 @@ def get_version():
             return "(Report bug if you see this)"
     else:
         return "(Report bug if you see this)"
+
+
+if __name__ == "__main__":
+    print(get_version())
